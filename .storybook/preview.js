71c6366f0a600f108015b07a55419ae01f8fbefd
Yunus M <myounis.ar@live.com>
2026-02-20 21:55:45 +0530

    feat: add Google Fonts integration and update default font in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,7 @@ const preview = {
     (Story) =>
       React.createElement(
         "div",
-        { style: { fontFamily: "system-ui, sans-serif" } },
+        { style: { fontFamily: "'Inter', system-ui, sans-serif" } },
         React.createElement(Story)
       ),
   ],
